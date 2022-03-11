ef0a6af74b11e7a4797a312155d424c927324646
René Fritze <rene.fritze@wwu.de>
2022-03-11 13:46:58 +0100

    format hooks

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 import sys
 
+
 def _validate_input():
     """Example (no double curlies to avoid rendering this):
     if not '{ cookiecutter.main_file }'.endswith('.tex'):
         print(f'"main_file" should end with ".tex"')
-        sys.exit(-1)
-"""
+        sys.exit(-1)"""
     pass
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     _validate_input()
