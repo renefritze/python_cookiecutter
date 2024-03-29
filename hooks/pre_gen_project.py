f5d060f54681cabc0b337994016aef1a1e3858af
René Fritze <rene.fritze@wwu.de>
2024-03-29 20:43:20 +0100

    fix pre-gen

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -6,7 +6,7 @@ EMAIL_REGEX = r"""(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-
 
 
 def _validate_input():
-    author_email = "{{ cookiecutter.author_email }}"
+    author_email = "{{ cookiecutter.email }}"
     if not re.match(EMAIL_REGEX, author_email):
         print(f"Invalid email: {author_email}")
         sys.exit(1)
