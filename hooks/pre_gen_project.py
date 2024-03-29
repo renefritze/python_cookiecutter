8c6a730ba4bab224151b8e68a7c4c1c4618d0404
René Fritze <rene.fritze@wwu.de>
2024-03-29 19:51:14 +0100

    hooks| check email before rendering the project

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python
+import re
+import sys
+
+EMAIL_REGEX = r"""(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])"""
 
 
 def _validate_input():
-    """Example (no double curlies to avoid rendering this):
-    if not '{ cookiecutter.main_file }'.endswith('.tex'):
-        print(f'"main_file" should end with ".tex"')
-        sys.exit(-1)"""
-    pass
+    author_email = "{{ cookiecutter.author_email }}"
+    if not re.match(EMAIL_REGEX, author_email):
+        print(f"Invalid email: {author_email}")
+        sys.exit(1)
 
 
 if __name__ == "__main__":
